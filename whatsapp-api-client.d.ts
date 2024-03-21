08e0ee13a0345fe3c3c83f09bedea12231395ed3
Andrey <andrishkin@gmail.com>
2024-03-21 09:04:26 +0500

    added: added some logic

--- whatsapp-api-client.d.ts
+++ whatsapp-api-client.d.ts
@@ -3,4 +3,12 @@ declare module '@green-api/whatsapp-api-client' {
     export function setSettings(): any;
     export function sendFileByUrl(): any;
 
+    declare module '@green-api/whatsapp-api-client' {
+        interface RestApiOptions {
+            idInstance: string;
+            apiTokenInstance: string;
+        }
+
+        export function restApi(options: RestApiOptions): any;
+    }
 }
