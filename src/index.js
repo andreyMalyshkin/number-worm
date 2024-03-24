3aa4e104099e163321577b10103f3404610abc17
Andrey <andrishkin@gmail.com>
2024-03-24 19:20:20 +0500

    added: added mongo to project

--- src/index.js
+++ src/index.js
@@ -1,33 +1,31 @@
 const express = require('express')
+const { Instance, connectDB } = require('./db')
 const cors = require('cors')
+require('dotenv').config()
 const whatsAppClient = require('@green-api/whatsapp-api-client')
 const initializeInstance = require('./initializeInstance.js')
-const utils = require('./utils.js')
+const utilsFunctions = require('./utils.js')
 
 const app = express()
 const PORT = 3000
 app.use(cors())
-const utilsFunctions = new utils()
+const utils = new utilsFunctions()
 
-const mainInstance = [
-  //TODO: расширить объекты инстансов, держать флаги и номера
-  {
-    idInstance: '9903878765',
-    token: 'd86433befe084e2398c8040a039d56b6e10cd2066d564f778c',
-    isOnline: false,
-  },
-  {
-    idInstance: '9903878766',
-    token: '17552f33bcdd4a178465ad000968167350ced97d07a5483bb2',
-    isOnline: false,
-  },
-]
+const mainInstance = [] //для получения и отправки
+const phoneNumbers = [] // только для получения
 
-const phoneNumbers = ['77059641582', '77071512603']
+async function loadDb() {
+  try {
+    const instances = await Instance.find()
+    mainInstance.push(...instances) // Это добавит все найденные документы в массив mainInstance
+    utils.logger.info('Data loaded from DB:', mainInstance)
+  } catch (error) {
+    utils.logger.error('Error loading data from DB:', error)
+  }
+}
 
 //Функция, которая отвечает за первичную работу инстансев, обновляется каждые пол часа ( вдруг пришли новые инстансы )
 async function main() {
-  //TODO: когда будет бд, сделать инициализацию бд и выгрузку в локальные переменные данных о инстансах и номерах
   for (let instance of mainInstance) {
     const restAPI = whatsAppClient.restAPI({
       idInstance: instance.idInstance,
@@ -37,7 +35,7 @@ async function main() {
     try {
       await initializeInstance(restAPI, instance)
     } catch (error) {
-      utilsFunctions.logger.error(error)
+      utils.logger.error(error)
       continue
       //TODO: описать логику пропуска инстанса и логику проверки ответа от сервера ( вдруг все норм, просто сервис упал )
     }
@@ -48,7 +46,16 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
 
-main().catch((error) => utilsFunctions.logger.error(error))
-setInterval(() => {
-  main().catch((error) => utilsFunctions.logger.error(error))
-}, 1800000)
+connectDB()
+  .then(() => {
+    utils.logger.info('Connected to MongoDB')
+    loadDb().then(() => {
+      main().catch((error) => utils.logger.error(error))
+      setInterval(() => {
+        main().catch((error) => utils.logger.error(error))
+      }, 1800000) // Повторяем каждые 30 минут
+    })
+  })
+  .catch((error) =>
+      utils.logger.error('Could not connect to MongoDB:', error),
+  )
--- src/initializeInstance.js
+++ src/initializeInstance.js
@@ -1,5 +1,6 @@
 const utilsFunctions = require('./utils.js')
 const utils = new utilsFunctions()
+const { Instance, connectDB } = require('./db')
 
 module.exports = async function initializeInstance(restApi, instance) {
   utils.logger.info(
@@ -15,21 +16,29 @@ module.exports = async function initializeInstance(restApi, instance) {
     if (data.stateInstance !== 'authorized') {
       utils.logger.error(`Instance ${instance.idInstance} is not online`)
       instance.isOnline = false
-      // TODO: необходимо описать логику, чтобы данные сохранялись в бд и передавался только номер
-    } else {
-      try {
-        const settingsResponse = await restApi.settings.setSettings(
-          utils.settings,
-        )
-        utils.logger.info(
-          `Instance ${instance.idInstance} save settings is ${settingsResponse.saveSettings}`,
-        )
-        instance.isOnline = true
-      } catch (error) {
-        utils.logger.error(
-          `Instance ${instance.idInstance} not take settings ${error}`,
-        )
-      }
+      await Instance.findOneAndUpdate(
+        { idInstance: instance.idInstance },
+        { $set: { isOnline: false } },
+      )
+      return
+    }
+
+    try {
+      const settingsResponse = await restApi.settings.setSettings(
+        utils.settings,
+      )
+      utils.logger.info(
+        `Instance ${instance.idInstance} save settings is ${settingsResponse.saveSettings}`,
+      )
+      instance.isOnline = true
+      await Instance.findOneAndUpdate(
+        { idInstance: instance.idInstance },
+        { $set: { isOnline: true } },
+      )
+    } catch (error) {
+      utils.logger.error(
+        `Instance ${instance.idInstance} not take settings ${error}`,
+      )
     }
   } catch (error) {
     utils.logger.error(
